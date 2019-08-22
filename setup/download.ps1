6abac4225d50a689136285090ab0214290f2119a
Ivo Beerens <ibeerens@gmail.com>
2019-08-22 14:47:39 +0200

    Update1

--- setup/download.ps1
+++ setup/download.ps1
@@ -1,23 +1,31 @@
-# Onedrive Kill
-taskkill.exe /F /IM "OneDrive.exe"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Copy Horizon client
-#$url = "http://192.168.13.131:81/VMware-Horizon-Agent-x86_64-7.9.0-13938590.exe"
-$url = "https://dl.dropboxusercontent.com/s/fd7k5z80dfb3b2t/VMware-Horizon-Agent-x86_64-7.9.0-13938590.exe"
+# Download Horizon Client
+$url = "https://www.ivobeerens.nl/VMware-Horizon-Agent-x86_64-7.9.0-13938590.msi"
 $output = "c:\install\VMware-Horizon-Agent-x86_64-7.9.0-13938590.exe"
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $output
-sleep 2
 
-# Install Horizon Agent
-# https://kb.vmware.com/s/article/1019862
-write-host "Install Horizon Agent"
-Start-Process -FilePath "c:\install\VMware-Horizon-Agent-x86_64-7.9.0-13938590.exe" -ArgumentList "/s /v /qn VDM_VC_MANAGED_AGENT=1 VDM_FLASH_URL_REDIRECTION=1 ADDLOCAL=ALL REBOOT=Reallysuppress" -Wait
-# InstallMSI -MSIName "VMware Horizon View Agent" -MSIArgs "/qb- REBOOT=ReallySuppress VDM_FLASH_URL_REDIRECTION=1 RDP_CHOICE=1 ADDLOCAL=Core,USB,NGVC,RTAV,ClientDriveRedirection,FlashURLRedirection,FLASHMMR,ThinPrint,V4V,VmwVaudio,TSMMR,RDP,VMWMediaProviderProxy"
-sleep 2
+# Download UEM Agent
+$url = "https://www.ivobeerens.nl/VMware User Environment Manager 9.8 x64.msi"
+$output = "c:\install\VMware User Environment Manager 9.8 x64.msi"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $output
 
-# Disable Hybernate
-powercfg.exe /h off
+# Download App Volumes Agent
+$url = "https://www.ivobeerens.nl/App Volumes Agent.msi"
+$output = "c:\install\App Volumes Agent.msi"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $output
 
-# Rename PC
-Rename-Computer -NewName w10-1809tmplt
+# Download Citrix optimzer  
+$url = "https://www.ivobeerens.nl/CitrixOptimizer.zip"
+$output = "c:\install\CitrixOptimzer.zip"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $output
+
+# Download BIS-F
+$url = "https://www.ivobeerens.nl/setup-BIS-F-6.1.3_build01.110.exe"
+$output = "c:\install\setup-BIS-F-6.1.3_build01.110.exe"
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $output
