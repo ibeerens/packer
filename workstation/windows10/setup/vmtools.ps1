03c2ee092b7282315c4c2af0aef0e5cfd76e6b2c
Ivo Beerens <ibeerens@gmail.com>
2022-05-28 21:52:57 +0200

    Add files via upload

--- workstation/windows10/setup/vmtools.ps1
+++ workstation/windows10/setup/vmtools.ps1
@@ -1,18 +1,24 @@
 ﻿$ErrorActionPreference = "SilentlyContinue"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# Create folder c:\install
-New-Item -Path 'c:\install' -ItemType Directory
+# Variables
+$downloadfolder = 'C:\install\'
 
-# Download VMware Tools
+# Create folder
+New-Item -Path $downloadfolder -ItemType Directory
+
+# Download the latest VMware Tools
 # Check the latest release by using the following link: 
 # https://packages.vmware.com/tools/releases/latest/windows/x64/
-$url = "https://packages.vmware.com/tools/releases/latest/windows/x64/VMware-tools-12.0.5-19716617-x86_64.exe"
-$output = "c:\install\VMware-tools-12.0.5-19716617-x86_64.exe"
-
-Invoke-WebRequest -Uri $url -UseBasicParsing -OutFile $output
+$url = "https://packages.vmware.com/tools/releases/latest/windows/x64/" 
+$vmwareurl = Invoke-WebRequest -Uri $url | Select-Object -ExpandProperty links | Where-Object HREF -Match "VM*" | Select-Object -ExpandProperty href
+$spliturl = $vmwareurl | Split-Path -Leaf
+$newurl = $url + $spliturl
+$downloadvmware = $downloadfolder + $spliturl
+$webclient = New-object -TypeName System.Net.WebClient
+$webclient.DownloadFile($newurl, $downloadvmware)
 
 # Install VMware Tools
 Start-Process -Wait `
-    -FilePath $output `
+    -FilePath $downloadvmware `
     -ArgumentList '/S /v"/qn REBOOT=R" /l c:\windows\temp\vmware_tools_install.log'
