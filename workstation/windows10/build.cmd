77844ca4d2a0de8472c80f62993463e3b17d4d25
Ivo Beerens <ibeerens@gmail.com>
2022-05-31 22:02:35 +0200

    Delete build.cmd

--- workstation/windows10/build.cmd
+++
@@ -1,5 +0,0 @@
-@ ECHO OFF
-CLS
-REM Make sure PowerShell 7 is installed 
-REM https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.2
-"C:\Program Files\PowerShell\7\pwsh.exe" -File C:\Packer\build.ps1
