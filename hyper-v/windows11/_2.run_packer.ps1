bbf6fa535bfcff1e5c49cc9b02be85c2ac7e0f5f
Ivo Beerens <info@ivobeerens.nl>
2023-08-31 11:42:22 +0200

    change

--- hyper-v/windows11/_2.run_packer.ps1
+++ hyper-v/windows11/_2.run_packer.ps1
@@ -1,3 +1,9 @@
+<#
+  Description: Download the latest version of Hashicorp Packer version and create a Windows 11 VM in Hyper-V
+  Created by: Ivo Beerens www.ivobeerens.nl
+  Change Log: Augustus 29, 2023 v1.0 Final version by Ivo Beerens
+#>
+
 # Show Packer Version
 .\packer.exe -v
 
